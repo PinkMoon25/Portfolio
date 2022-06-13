f174210c18ec0d624b18c433a112321b6544346d
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-06-13 16:59:50 +0100

    Stylent js and css error

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const dropdown = document.querySelector('.dropdown');
 const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.close-modal')
+const closeModal = document.querySelector('.close-modal');
 const links = document.querySelectorAll('.modal-links');
 
 dropdown.addEventListener('click', () => {
   modal.style.display = 'block';
-})
+});
 
 closeModal.addEventListener('click', () => {
   modal.style.display = 'none';
-})
+});
 
-
-for(let i=0; i<links.length; i++){
+for (let i = 0; i < links.length; i = i+1) {
   links[i].addEventListener('click', () => {
     modal.style.display = 'none';
-  })
+  });
 }
