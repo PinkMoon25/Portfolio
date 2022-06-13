e6e077404cd89daf30cbf746db9b84df42b7e2ba
Tushar <singhtushar348@gmail.com>
2022-06-13 16:26:09 +0100

    Add styling and function to the dropdown menu

---
+++ script.js
@@ -0,0 +1,19 @@
+const dropdown = document.querySelector('.dropdown');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-modal')
+const links = document.querySelectorAll('.modal-links');
+
+dropdown.addEventListener('click', () => {
+  modal.style.display = 'block';
+})
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+})
+
+
+for(let i=0; i<links.length; i++){
+  links[i].addEventListener('click', () => {
+    modal.style.display = 'none';
+  })
+}
