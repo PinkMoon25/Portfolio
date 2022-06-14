0d2d347e724e8b0cc73d5b3e91196354560d6298
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-06-14 18:20:51 +0100

    Fix linteter error

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ dropdown.addEventListener('click', () => {
 
 closeModal.addEventListener('click', () => {
   modal.style.display = 'none';
-  popupContainer.style.display = 'none';
 });
 
 for (let i = 0; i < links.length; i += 1) {
@@ -18,64 +17,60 @@ for (let i = 0; i < links.length; i += 1) {
   });
 }
 
-const works = document.querySelector('.works');
 const projects = document.querySelector('.projects');
-const multiPost = document.querySelector('.multi-post');
-const projectCardOne = document.querySelector('.project-card');
-const projectCards = document.querySelector('.project-card.two');
-
 
 const article = document.createElement('article');
 article.classList.add('multi-post');
-article.innerHTML = `<img src="./images/multi-post.png" alt="project image" class="project-image">
-<div class="multi-post content">
-<h1 class="post-title">Multi Post Stories</h1>
-<p class="post-description">A daily selection of privately personalized reads; no accounts or sign-ups required.
+article.innerHTML = `<img src='./images/multi-post.png' alt='project image' class='project-image'>
+<div class='multi-post content'>
+<h1 class='post-title'>Multi Post Stories</h1>
+<p class='post-description'>A daily selection of privately personalized reads; no accounts or sign-ups required.
   has been the industry's standard dummy text ever since the 1500s, 
   when an unknown printer took a standard dummy text.</p>
-  <ul class="article-tags">
+  <ul class='article-tags'>
     <li>Css</li>
     <li>html</li>
     <li>bootstrap</li>
     <li>Ruby</li>
   </ul>
-<button type="button" class="article-btn">See Project</button>
+<button type='button' class='article-btn'>See Project</button>
 </div>`
 
 projects.appendChild(article);
 
-function addProjectCards () {
-  for(let i = 0; i < 6; i += 1){
+function addProjectCards() {
+  for (let i = 0; i < 6; i += 1) {
     const div = document.createElement('div');
     div.classList.add('project-card');
-    if(i == 0){
+    if (i == 0) {
       div.classList.add('one');
     }
-    else if(i == 1){
+    else if (i == 1) {
       div.classList.add('two');
     }
-    else if(i == 2){
+    else if (i == 2) {
       div.classList.add('three');
     }
-    else if(i == 3){
+    else if (i == 3) {
       div.classList.add('four');
     }
-    else if(i == 4){
+    else if (i == 4) {
       div.classList.add('five');
     }
-    else if(i == 5){
+    else if (i == 5) {
       div.classList.add('six');
     }
-    div.innerHTML = `<h1 class="project-title">Professional Art Printing Data</h1>
-    <p class="project-description">A daily selection of privately personalized reads; 
+
+    div.innerHTML = `<h1 class='project-title'>Professional Art Printing Data</h1>
+    <p class='project-description'>A daily selection of privately personalized reads; 
       no accounts or sign-ups required.
       has been the industry's standard</p>
-    <ul class="project-tags">
+    <ul class='project-tags'>
       <li>html</li>
       <li>bootstrap</li>
       <li>Ruby</li>
     </ul>
-    <button type="button" class="project-btn">See Project</button>`
+    <button type='button' class='project-btn'>See Project</button>`
     projects.appendChild(div);
   }
 }
@@ -84,116 +79,116 @@ addProjectCards();
 
 const popupArray = [
   {
-    name: `<h1 class="post-title">Multi Post Stories</h1>`,
-    closeBtn: `<span class="close-popup"><i class="fa-solid fa-xmark"></i></span>`,
-    technologies: `<ul class="article-tags">
+    name: `<h1 class='post-title'>Multi Post Stories</h1>`,
+    closeBtn: `<span class='close-popup'><i class='fa-solid fa-xmark'></i></span>`,
+    technologies: `<ul class='article-tags'>
     <li>Css</li>
     <li>html</li>
     <li>bootstrap</li>
     <li>Ruby</li>
   </ul>`,
-    image: `<img src="./images/multi-post.png" alt="project image" class="project-image">`,
-    description: `<p class="post-description">A daily selection of privately personalized reads; no accounts or sign-ups required.
+    image: `<img src='./images/multi-post.png' alt='project image' class='project-image'>`,
+    description: `<p class='post-description'>A daily selection of privately personalized reads; no accounts or sign-ups required.
     has been the industry's standard dummy text ever since the 1500s, 
     when an unknown printer took a standard dummy text.</p>`,
-    live: `<button type="button" class="article-btn">See Live<img src="Icons/Iconlive.png"></button>`,
-    Source: `<button type="button" class="article-btn">See Source<i class="fa-brands fa-github"></button>`
+    live: `<button type='button' class='article-btn'>See Live<img src='Icons/Iconlive.png'></button>`,
+    Source: `<button type='button' class='article-btn'>See Source<i class='fa-brands fa-github'></button>`
   },
 
   {
-    name: `<h1 class="project-title">Professional Art Printing Data 1</h1>`,
-    closeBtn: `<span class="close-popup"><i class="fa-solid fa-xmark"></i></span>`,
-    technologies: `<ul class="project-tags">
+    name: `<h1 class='project-title'>Professional Art Printing Data 1</h1>`,
+    closeBtn: `<span class='close-popup'><i class='fa-solid fa-xmark'></i></span>`,
+    technologies: `<ul class='project-tags'>
     <li>html</li>
     <li>bootstrap</li>
     <li>Ruby</li>
   </ul>`,
-    image: `<img src="./images/multi-post.png" alt="project image" class="project-image">`,
-    description: `<p class="project-description">A daily selection of privately personalized reads; 
+    image: `<img src='./images/multi-post.png' alt='project image' class='project-image'>`,
+    description: `<p class='project-description'>A daily selection of privately personalized reads; 
     no accounts or sign-ups required.
     has been the industry's standard</p>`,
-    live: `<button type="button" class="project-btn">See Live<img src="Icons/Iconlive.png"></button>`,
-    Source: `<button type="button" class="project-btn">See Source<i class="fa-brands fa-github"></i></button>`
+    live: `<button type='button' class='project-btn'>See Live<img src='Icons/Iconlive.png'></button>`,
+    Source: `<button type='button' class='project-btn'>See Source<i class='fa-brands fa-github'></i></button>`
   },
 
   {
-    name: `<h1 class="project-title">Professional Art Printing Data 2</h1>`,
-    closeBtn: `<span class="close-popup"><i class="fa-solid fa-xmark"></i></span>`,
-    technologies: `<ul class="project-tags">
+    name: `<h1 class='project-title'>Professional Art Printing Data 2</h1>`,
+    closeBtn: `<span class='close-popup'><i class='fa-solid fa-xmark'></i></span>`,
+    technologies: `<ul class='project-tags'>
     <li>html</li>
     <li>bootstrap</li>
     <li>Ruby</li>
   </ul>`,
-    image: `<img src="./images/multi-post.png" alt="project image" class="project-image">`,
-    description: `<p class="project-description">A daily selection of privately personalized reads; 
+    image: `<img src='./images/multi-post.png' alt='project image' class='project-image'>`,
+    description: `<p class='project-description'>A daily selection of privately personalized reads; 
     no accounts or sign-ups required.
     has been the industry's standard</p>`,
-    live: `<button type="button" class="project-btn">See Live<img src="Icons/Iconlive.png"></button>`,
-    Source: `<button type="button" class="project-btn">See Source<i class="fa-brands fa-github"></i></button>`
+    live: `<button type='button' class='project-btn'>See Live<img src='Icons/Iconlive.png'></button>`,
+    Source: `<button type='button' class='project-btn'>See Source<i class='fa-brands fa-github'></i></button>`
   },
   
   {
-    name: `<h1 class="project-title">Professional Art Printing Data 3</h1>`,
-    closeBtn: `<span class="close-popup"><i class="fa-solid fa-xmark"></i></span>`,
-    technologies: `<ul class="project-tags">
+    name: `<h1 class='project-title'>Professional Art Printing Data 3</h1>`,
+    closeBtn: `<span class='close-popup'><i class='fa-solid fa-xmark'></i></span>`,
+    technologies: `<ul class='project-tags'>
     <li>html</li>
     <li>bootstrap</li>
     <li>Ruby</li>
   </ul>`,
-    image: `<img src="./images/multi-post.png" alt="project image" class="project-image">`,
-    description: `<p class="project-description">A daily selection of privately personalized reads; 
+    image: `<img src='./images/multi-post.png' alt='project image' class='project-image'>`,
+    description: `<p class='project-description'>A daily selection of privately personalized reads; 
     no accounts or sign-ups required.
     has been the industry's standard</p>`,
-    live: `<button type="button" class="project-btn">See Live<img src="Icons/Iconlive.png"></button>`,
-    Source: `<button type="button" class="project-btn">See Source<i class="fa-brands fa-github"></i></button>`
+    live: `<button type='button' class='project-btn'>See Live<img src='Icons/Iconlive.png'></button>`,
+    Source: `<button type='button' class='project-btn'>See Source<i class='fa-brands fa-github'></i></button>`
   },
 
   {
-    name: `<h1 class="project-title">Professional Art Printing Data 4</h1>`,
-    closeBtn: `<span class="close-popup"><i class="fa-solid fa-xmark"></i></span>`,
-    technologies: `<ul class="project-tags">
+    name: `<h1 class='project-title'>Professional Art Printing Data 4</h1>`,
+    closeBtn: `<span class='close-popup'><i class='fa-solid fa-xmark'></i></span>`,
+    technologies: `<ul class='project-tags'>
     <li>html</li>
     <li>bootstrap</li>
     <li>Ruby</li>
   </ul>`,
-    image: `<img src="./images/multi-post.png" alt="project image" class="project-image">`,
-    description: `<p class="project-description">A daily selection of privately personalized reads; 
+    image: `<img src='./images/multi-post.png' alt='project image' class='project-image'>`,
+    description: `<p class='project-description'>A daily selection of privately personalized reads; 
     no accounts or sign-ups required.
     has been the industry's standard</p>`,
-    live: `<button type="button" class="project-btn">See Live<img src="Icons/Iconlive.png"></button>`,
-    Source: `<button type="button" class="project-btn">See Source<i class="fa-brands fa-github"></i></button>`
+    live: `<button type='button' class='project-btn'>See Live<img src='Icons/Iconlive.png'></button>`,
+    Source: `<button type='button' class='project-btn'>See Source<i class='fa-brands fa-github'></i></button>`
   },
 
   {
-    name: `<h1 class="project-title">Professional Art Printing Data 5</h1>`,
-    closeBtn: `<span class="close-popup"><i class="fa-solid fa-xmark"></i></span>`,
-    technologies: `<ul class="project-tags">
+    name: `<h1 class='project-title'>Professional Art Printing Data 5</h1>`,
+    closeBtn: `<span class='close-popup'><i class='fa-solid fa-xmark'></i></span>`,
+    technologies: `<ul class='project-tags'>
     <li>html</li>
     <li>bootstrap</li>
     <li>Ruby</li>
   </ul>`,
-    image: `<img src="./images/multi-post.png" alt="project image" class="project-image">`,
-    description: `<p class="project-description">A daily selection of privately personalized reads; 
+    image: `<img src='./images/multi-post.png' alt='project image' class='project-image'>`,
+    description: `<p class='project-description'>A daily selection of privately personalized reads; 
     no accounts or sign-ups required.
     has been the industry's standard</p>`,
-    live: `<button type="button" class="project-btn">See Live<img src="Icons/Iconlive.png"></button>`,
-    Source: `<button type="button" class="project-btn">See Source<i class="fa-brands fa-github"></i></button>`
+    live: `<button type='button' class='project-btn'>See Live<img src='Icons/Iconlive.png'></button>`,
+    Source: `<button type='button' class='project-btn'>See Source<i class='fa-brands fa-github'></i></button>`
   },
 
   {
-    name: `<h1 class="project-title">Professional Art Printing Data 6</h1>`,
-    closeBtn: `<span class="close-popup"><i class="fa-solid fa-xmark"></i></span>`,
-    technologies: `<ul class="project-tags">
+    name: `<h1 class='project-title'>Professional Art Printing Data 6</h1>`,
+    closeBtn: `<span class='close-popup'><i class='fa-solid fa-xmark'></i></span>`,
+    technologies: `<ul class='project-tags'>
     <li>html</li>
     <li>bootstrap</li>
     <li>Ruby</li>
   </ul>`,
-    image: `<img src="./images/multi-post.png" alt="project image" class="project-image">`,
-    description: `<p class="project-description">A daily selection of privately personalized reads; 
+    image: `<img src='./images/multi-post.png' alt='project image' class='project-image'>`,
+    description: `<p class='project-description'>A daily selection of privately personalized reads; 
     no accounts or sign-ups required.
     has been the industry's standard</p>`,
-    live: `<button type="button" class="project-btn">See Live<img src="Icons/Iconlive.png"></button>`,
-    Source: `<button type="button" class="project-btn">See Source<i class="fa-brands fa-github"></i></button>`
+    live: `<button type='button' class='project-btn'>See Live<img src='Icons/Iconlive.png'></button>`,
+    Source: `<button type='button' class='project-btn'>See Source<i class='fa-brands fa-github'></i></button>`
   },
 ]
 
@@ -203,27 +198,27 @@ const popupContent = document.createElement('article');
 
 function popup(i){
   popupContent.innerHTML = '';
-    if( i == 0){
+    if ( i === 0) {
       Object.values(popupArray[0]).forEach(item => popupContent.innerHTML += item);      
     }
-    else if( i == 1){
+    else if ( i === 1) {
       Object.values(popupArray[1]).forEach(item => {
         popupContent.innerHTML += item;
       });  
     }
-    else if( i == 2){
+    else if ( i === 2) {
       Object.values(popupArray[2]).forEach(item => popupContent.innerHTML += item);  
     }
-    else if( i == 3){
+    else if ( i === 3) {
       Object.values(popupArray[3]).forEach(item => popupContent.innerHTML += item);  
     }
-    else if( i == 4){
+    else if ( i === 4) {
       Object.values(popupArray[4]).forEach(item => popupContent.innerHTML += item); 
     }
-    else if( i == 5){
+    else if ( i === 5) {
       Object.values(popupArray[5]).forEach(item => popupContent.innerHTML += item); 
     }
-    else if( i == 6){
+    else if ( i === 6) {
       Object.values(popupArray[6]).forEach(item => popupContent.innerHTML += item); 
     }
   popupContainer.classList.add('popup-container');
@@ -247,8 +242,8 @@ articleBtn.addEventListener('click', () => {
   const closePopup = document.querySelector('.close-popup');
   closePopup.addEventListener('click', () => {
     popupContainer.style.display = 'none';
-  })
-})
+  });
+});
 
 buttonOne.addEventListener('click', () => {
   popup(1);
@@ -256,9 +251,8 @@ buttonOne.addEventListener('click', () => {
   const closePopup = document.querySelector('.close-popup');
   closePopup.addEventListener('click', () => {
     popupContainer.style.display = 'none';
-  })
-})
-
+  });
+});
 
 buttonTwo.addEventListener('click', () => {
   popup(2);
@@ -266,9 +260,8 @@ buttonTwo.addEventListener('click', () => {
   const closePopup = document.querySelector('.close-popup');
   closePopup.addEventListener('click', () => {
     popupContainer.style.display = 'none';
-  })
-})
-
+  });
+});
 
 buttonThree.addEventListener('click', () => {
   popup(3);
@@ -276,9 +269,8 @@ buttonThree.addEventListener('click', () => {
   const closePopup = document.querySelector('.close-popup');
   closePopup.addEventListener('click', () => {
     popupContainer.style.display = 'none';
-  })
-})
-
+  });
+});
 
 buttonFour.addEventListener('click', () => {
   popup(4);
@@ -286,9 +278,8 @@ buttonFour.addEventListener('click', () => {
   const closePopup = document.querySelector('.close-popup');
   closePopup.addEventListener('click', () => {
     popupContainer.style.display = 'none';
-  })
-})
-
+  });
+});
 
 buttonFive.addEventListener('click', () => {
   popup(5);
@@ -296,9 +287,8 @@ buttonFive.addEventListener('click', () => {
   const closePopup = document.querySelector('.close-popup');
   closePopup.addEventListener('click', () => {
     popupContainer.style.display = 'none';
-  })
-})
-
+  });
+});
 
 buttonSix.addEventListener('click', () => {
   popup(6);
@@ -306,5 +296,5 @@ buttonSix.addEventListener('click', () => {
   const closePopup = document.querySelector('.close-popup');
   closePopup.addEventListener('click', () => {
     popupContainer.style.display = 'none';
-  })
-})
+  });
+});
